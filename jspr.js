3d53547a1ce6d2abb10bae4cb588ccb5c88ac075
clarkjoypesco@yahoo.com <clarkjoypesco@yahoo.com>
2019-04-16 16:10:06 +0800

    function level scope

--- jspr.js
+++ jspr.js
@@ -2,12 +2,12 @@ var outsideExample = "First string";
 function example() {
     var outsideExample = "Second string";
 }
-example();
-console.log(outsideExample);
+example(); // returns noting
+console.log(outsideExample); // prints first string
 
 var outsideExample = "First string";
 function example() {
     outsideExample = "Second string";
 }
-example();
-console.log(outsideExample);
+example(); // change the value of outsideExxample to Second String
+console.log(outsideExample); // prints Second String
